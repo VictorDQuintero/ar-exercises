f9edf0a4c000bc3916de8b403a8e697f957ccfb8
VictorDQuintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-10-28 17:52:09 -0600

    Remove commented out code

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,11 +1,5 @@
 require_relative '../setup'
 
-=begin 
- Burnaby (annual_revenue of 300000, carries men's and women's apparel)
-- Richmond (annual_revenue of 1260000 carries women's apparel only)
-- Gastown (annual_revenue of 190000 carries men's apparel only)
-=end
-
 puts "Exercise 1"
 puts "----------"
 
@@ -14,4 +8,4 @@ Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, wom
 Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
 puts Store.count
-# Your code goes below here ...
+
